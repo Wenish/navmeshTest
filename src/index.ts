8acdb9efdf750fd99a03e08ac85333d3561f3a18
Jonas <noreply@jonas.com>
2020-03-03 16:56:55 +0100

    finish test 1

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,46 @@ fs.readFile('./src/map.obj', 'utf8', function(err, data) {
     fs.writeJson('./src/map.json', parsedJSON, {
         spaces: 2
     })
+    /*
+    const meshPolygonPoints = [
+        [{ x: 0, y: 0 }, { x: 10, y: 0 }, { x: 10, y: 10 }, { x: 0, y: 10 }], // Polygon 1
+        [{ x: 10, y: 0 }, { x: 20, y: 0 }, { x: 20, y: 10 }, { x: 10, y: 10 }], // Polygon 2
+        [{ x: 10, y: 10 }, { x: 20, y: 10 }, { x: 20, y: 20 }, { x: 10, y: 20 }] // Polygon 3
+    ];
+    */
+    const meshPolygonPoints = []
+    const vertexPositions = parsedJSON.vertexPositions
+    const array = parsedJSON.vertexPositionIndices
+    for (let index = 0; index < array.length; index += 4) {
+        const aIndex = array[index];
+        const bIndex = array[index + 1]
+        const cIndex = array[index + 2]
+        meshPolygonPoints.push([
+            {
+                x: vertexPositions[aIndex * 3],
+                y: vertexPositions[(aIndex * 3) + 2]
+            },
+            {
+                x: vertexPositions[bIndex * 3],
+                y: vertexPositions[(bIndex * 3) + 2]
+            },
+            {
+                x: vertexPositions[cIndex * 3],
+                y: vertexPositions[(cIndex * 3) + 2]
+            }            
+        ])
+    }
+    fs.writeJson('./src/meshPolygonPoints.json', meshPolygonPoints, {
+        spaces: 2
+    })
+
+    const navMesh = new NavMesh(meshPolygonPoints);
+    console.log(navMesh)
+    const path = navMesh.findPath({ x: 3, y: 2 }, { x: 8, y: 8 });
+    console.log(path)
+    fs.writeJson('./src/path.json', path, {
+        spaces: 2
+    })
 });
 
 
