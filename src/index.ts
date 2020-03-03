4da593a17c6989add18e06eb7b4163ff4e34c057
Jonas <noreply@jonas.com>
2020-03-03 16:13:36 +0100

    init

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log('Hallo from TS')
+import fs from 'fs-extra'
+import wavefrontObjParser from 'wavefront-obj-parser'
+import NavMesh from "navmesh";
+
+fs.readFile('./src/map.obj', 'utf8', function(err, data) {
+    if (err) throw err;
+    console.log(data);
+    var parsedJSON = wavefrontObjParser(data)
+    console.log(parsedJSON)
+    fs.writeJson('./src/map.json', parsedJSON)
+});
+
+
+//import NavMesh from "navmesh"
+/*
+const navMesh = new NavMesh([])
+console.log(navMesh)
+*/
